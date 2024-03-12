fd01cb83741e74dae5be5bfc5d5247a2f01d9ee4
VictorNTW <victor1992hm@gmail.com>
2024-03-12 19:27:55 -0300

    [UPDATED] HTML5

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php echo "Hello World";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VictorNTW</title>
+</head>
+<body>
+    <?php echo "Hello World";?>
+    
+</body>
+</html>
