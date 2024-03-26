cf64bd85b887f8332d111e42624b7f873a4a3730
VictorNTW <victor1992hm@gmail.com>
2024-03-26 17:23:44 -0300

    [UPDATE] Separando Responsabilidades

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VictorNTW</title>
-</head>
-<body>
-    <?php echo "Hello World";?>
+<?php
+
+    include 'assets/html/home.html';
     
-</body>
-</html>
